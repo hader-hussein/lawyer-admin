776fee4640134097fc6f7bd9a82c25dfa3b33b00
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2024-05-15 14:54:01 +0300

    15-5-2024

--- assets/vendor/js/accordion.js
+++ assets/vendor/js/accordion.js
@@ -37,5 +37,38 @@ function optionCheck() {
    }
 }
 /************************ */
-
-
+function addField(n)
+  {
+      var tr = n.parentNode.parentNode.cloneNode(true);
+      document.getElementById('tbl').appendChild(tr);
+      var removeButton = tr.getElementsByTagName("button")[1];
+      removeButton.parentNode.removeChild(removeButton);
+    
+  }
+  $(document).ready(function(){
+    $("#my_table").on('click','.btnDelete',function(){
+        $(this).closest('tr').remove();
+     });
+     
+ });
+ function show1(){
+    document.getElementById('div1').style.display ='none';
+  }
+  function show2(){
+    document.getElementById('div1').style.display = 'block';
+  }
+ /**********profil-img********** */
+ function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+          $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+          $('#imagePreview').hide();
+          $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+$("#imageUpload").change(function() {
+  readURL(this);
+});
